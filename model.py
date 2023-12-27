3f84ead7051cbedcf64effc998cd154f28e2c43f
Daniel Alejandro Vidal Guerra <dani@iMac-de-Daniel.local>
2023-12-27 15:34:00 +0100

    upload changes

---
+++ model.py
@@ -0,0 +1,24 @@
+import requests
+from PIL import Image
+from transformers import BlipProcessor, BlipForConditionalGeneration
+
+processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
+
+#img_url = 'https://storage.googleapis.com/sfr-vision-language-research/BLIP/demo.jpg' 
+#raw_image = Image.open(requests.get(img_url, stream=True).raw).convert('RGB')
+ 
+# conditional image captioning
+def conditional_caption_image(image_path, text):
+    raw_image = Image.open(image_path).convert('RGB')
+    inputs = processor(raw_image, text, return_tensors="pt")
+    out = model.generate(**inputs)
+    return processor.decode(out[0], skip_special_tokens=True)
+
+# unconditional image captioning
+def caption_image(image_path):
+    raw_image = Image.open(image_path).convert('RGB')
+    inputs = processor(raw_image, return_tensors="pt")
+    out = model.generate(**inputs)
+    return processor.decode(out[0], skip_special_tokens=True)
+
